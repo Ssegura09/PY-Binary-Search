a87e36b4368127da144bd1931adbf6604380f01c
Stephanie Segura <Steph@Stephanies-MBP.attlocal.net>
2022-06-14 20:23:14 -0500

    Completed binary search

---
+++ binary_search.py
@@ -0,0 +1,21 @@
+def binary_search(data, el):
+  first = 0
+#   Note that last = len(data)-1 because indexing starts at 0.
+  last = len(data)-1 
+  found = False
+
+  while first <= last and not found:
+      mid = (first + last) // 2
+
+      if data[mid] == el:
+          found = True
+      else:
+          if el < data[mid]:
+              last = mid - 1
+          else:
+              first = mid + 1
+  return found
+
+test_list = [5, 8, 12, 14, 19, 22, 27, 30, 31]
+print(binary_search(test_list, 12))
+print(binary_search(test_list, 11))
